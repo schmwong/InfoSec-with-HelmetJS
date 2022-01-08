8b6579643d435b521b7ec4174de741d7bce159b7
Paul Wong <79643071+schmwong@users.noreply.github.com>
2022-01-08 19:59:00 +0800

    install and require helmet helmet version 3.21.3

--- myApp.js
+++ myApp.js
@@ -1,59 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Install Helmet version 3.21.3,
+// then require it. You can install
+// a specific version of a package with
+// npm install --save-exact package@version,
+// or by adding it to your package.json directly.
+let helmet = require("helmet");
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
